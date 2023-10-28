8a46b5fb2c688f7794513fa6f999b07a8cdfa1f6
pbon7477 <144088967+pbon7477@users.noreply.github.com>
2023-10-27 23:57:45 -0400

    Update db.js

--- database/db.js
+++ database/db.js
@@ -4,8 +4,7 @@ const conexion = mysql.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
-    port:process.env.DB_PORT
+    database:process.env.DB_DATABASE
 });
 
 conexion.connect((error)=>{
